41d95c399dfd6214d59154f2551ac545eb297232
Mevlüt Atila GÜNEŞ <mgunes@innova.com.tr>
2022-03-22 11:44:59 +0300

    First Commit

---
+++ src/main/java/tr/com/odamede/App.java
@@ -0,0 +1,13 @@
+package tr.com.odamede;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
